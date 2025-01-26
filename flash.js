1a165d6f6b64d960fcac314a99b951258bc16f92
davidaseyo <aseyodavid@gmail.com>
2025-01-26 16:14:49 +0200

    Create flash.js

---
+++ flash.js
@@ -0,0 +1,14 @@
+let batman= document.getElementById('damn')
+
+let amount=0
+
+function add(){
+amount=amount+1
+batman.innerHTML=amount
+}
+
+
+function take(){
+    amount=amount*0
+    batman.innerHTML=amount
+}
